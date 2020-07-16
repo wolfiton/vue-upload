e23cf33bbe08335c29e2d5aec93fc24f134b6a46
lorado <eugen.sacharow@gmx.de>
2020-07-17 00:03:34 +0200

    add an additional v-if to avoid error

--- vue-upload-client/src/views/Home.vue
+++ vue-upload-client/src/views/Home.vue
@@ -6,35 +6,37 @@
                 <!-- Some content -->
                 <div v-if="loading">Loading...</div>
                 <div v-else class="row">
-                    <div
-                        v-for="post of data.posts.data"
-                        :key="post.id"
-                        class="col-12 col-md-6 col-lg-4 mt-2"
-                    >
-                        <b-card
-                            border-variant="primary"
-                            header-bg-variant="primary"
-                            header-text-variant="white"
-                            :header="post.title"
-                            style="height:400px;"
+                    <template v-if="data">
+                        <div
+                            v-for="post of data.posts.data"
+                            :key="post.id"
+                            class="col-12 col-md-6 col-lg-4 mt-2"
                         >
-                            <b-card-text>
-                                <img
-                                    :src="
-                                        `http://localhost:8000/storage/${post.image}`
-                                    "
-                                    alt="cover image"
-                                    style="max-width:200px; max-height:300px;"
-                                    class="rounded"
-                                />
-                                <div>{{ post.content }}</div>
-                            </b-card-text>
-
-                            <b-button href="#" variant="primary"
-                                >Go somewhere</b-button
+                            <b-card
+                                border-variant="primary"
+                                header-bg-variant="primary"
+                                header-text-variant="white"
+                                :header="post.title"
+                                style="height:400px;"
                             >
-                        </b-card>
-                    </div>
+                                <b-card-text>
+                                    <img
+                                        :src="
+                                            `http://localhost:8000/storage/${post.image}`
+                                        "
+                                        alt="cover image"
+                                        style="max-width:200px; max-height:300px;"
+                                        class="rounded"
+                                    />
+                                    <div>{{ post.content }}</div>
+                                </b-card-text>
+
+                                <b-button href="#" variant="primary"
+                                    >Go somewhere</b-button
+                                >
+                            </b-card>
+                        </div>
+                    </template>
                 </div>
             </template>
         </ApolloQuery>
